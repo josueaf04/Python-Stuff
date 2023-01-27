1e81fe064938a818af746dc8da087d0416cb2be3
davidangulofz@gmail.com <davidangulofz@gmail.com>
2023-01-27 17:12:22 -0600

    exercise 2 finished

--- Homework/ejercicio_2.py
+++ Homework/ejercicio_2.py
@@ -1,1 +1,25 @@
 "Triangulo"
+
+# Problema: Crear un programa cuyo output genere un patrón en forma de triángulo que finalice al llegar al input.
+
+# Planeación: La interfaz del programa será la consola de comando, la función del program será crear un patrón en forma de triángulo
+# finalizando este en el número que el usuario ingresó.
+
+#Pseudocódigo
+# El usuario ingresa un número mayor a 0.
+# El programa genera un loop que empieza en 1 y termina en el input.
+# Imprime "" para que cada vez que "i" se itera, sea en una línea distinta (hacia abajo).
+# Luego el programa crea un loop (dentro del loop ya existente) que va desde 1 hasta la variable "i".
+# Finalmente imprime "j" y declara que ese loop termina en " " para evitar que los números de "j" se ejecuten
+# sin espacio 
+
+
+num = int(input("Ingrese un número mayor a 0: ")) 
+for i in range(1, num + 1): 
+    print("")
+    for j in range (1, i+ 1): 
+        print(j, end = " ") 
+
+
+
+    
