c9b57ddeb2961df52fde97c588c8ec4851ab692f
davidangulofz@gmail.com <davidangulofz@gmail.com>
2023-01-27 14:10:49 -0600

    process

--- Homework/ejercicio_1.py
+++ Homework/ejercicio_1.py
@@ -1,3 +1,24 @@
 "Factorial del numero dado"
 
+# Problema: Crear un programa que dé como resultado el factorial del número ingresado por el usuario (input).
+
+# Planeación: La interfaz del programa será la consola de comando, su función dar como resultado el factorial del número ingresado por el usuario.
+
+# Pseudocódigo: 
+# El usuario ingresa un número entero al programa.
+# El programa genera un loop que va desde la variable "num" y termina en el input.
+# La variable "factorial" se multiplica por cada uno de los números contenidos en el loop.
+# Dando como resultado final e imprimiendo el factorial de número ingresado por el usuario.
+
+# Subproblemas: 
+# Generar las variables "factorial" y "num" con un valor = 1.
+# Crear un loop "for _ in range" para multiplicar "factorial" * los números contenidos en el loop.
+# El programa debe imprimir el factorial correcto.
+
+
+
+
+ 
+
+
 
