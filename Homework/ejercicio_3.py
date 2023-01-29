370cb32d178e353286f5dd5e42e186f6ab2a402b
davidangulofz@gmail.com <davidangulofz@gmail.com>
2023-01-29 17:03:06 -0600

    Ex 3

--- Homework/ejercicio_3.py
+++ Homework/ejercicio_3.py
@@ -1,13 +1,21 @@
 "Strings intercaladas"
 
+# Problema: Crear un programa que reciba dos palabras del mismo largo y que cree una nueva con los caracteres intercalados. 
+
+# La interfaz del programa será la consola de comando, la función del programa será recibir dos palabras del mismo largo y crear
+# una string con los caracteres de esas palabras intercalados.
+
+# Pseudocódigo: 
+# Se crean 2 inputs para que el usuario ingrese las 2 palabras.
+# Mediante una validación "if" el programa analiza si las palabras contienen el mismo número de caracteres.
+# Genera una variable en la cual se concatenan los dos inputs.
+
 str = input("Ingrese una palabra: ")
 str2 = input("Ingrese otra palabra que contenga los mismos digitos que la anterior: ")
 if len(str) != len(str2): 
     print("Error, las palabras deben de tener el mismo número de digitos.")
-for i in str, str2: 
-    
-    
+x = [str + str2] 
 
 
-     
-    
+
+
