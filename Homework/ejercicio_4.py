28babf9c0ebe78427186fd6b3f891a73e650ff44
davidangulofz@gmail.com <davidangulofz@gmail.com>
2023-01-28 23:24:11 -0600

    Exercise 4 finished

--- Homework/ejercicio_4.py
+++ Homework/ejercicio_4.py
@@ -1,9 +1,26 @@
 "Lista al cubo"
 
+# Problema: Crear un programa que eleve los digitos de una lista al cubo, en una nueva lista.
+
+# La interfaz del programa será la consola de comando, el programa recibirá una lista con ciertos numeros, el output deseado es que el programa cree una
+# nueva lista pero con el resultado del cubo de estos numeros. 
+
+# Pseudocódigo: 
+# El programa recibe una lista con numeros.
+# Crea una lista vacía en la cuál irá el output que se busca.
+# Se genera un loop "for" con variable "num" para iterar sobre la lista con los numeros.
+# Cada vez que num pase por "list" eleva los valores al cubo.
+# Utilizando append() se agrega la variable "num" a la lista vacía.
+# Imprime el output. 
+
+# Subproblemas: Crear la lista vacía, desarrollar el loop for para iterar sobre la lista con los numeros, dentro de ese loop elevar la variable al cubo, 
+# añadir la variable a la lista vacía e imprimir.
+
+# Resolución: 
+
 list = [1, 2, 3]
-print(list)
-cubo = []
+output = []
 for num in list: 
     num **= 3 
-    cubo.append(num)
-print(cubo) 
+    output.append(num)
+print(output) 
