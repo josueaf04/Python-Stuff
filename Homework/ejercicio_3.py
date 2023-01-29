5d6b09b051bb32f46918acf12c803e190d45a3b3
davidangulofz@gmail.com <davidangulofz@gmail.com>
2023-01-29 17:13:04 -0600

    ex 3

--- Homework/ejercicio_3.py
+++ Homework/ejercicio_3.py
@@ -10,6 +10,8 @@
 # Mediante una validación "if" el programa analiza si las palabras contienen el mismo número de caracteres.
 # Genera una variable en la cual se concatenan los dos inputs.
 
+# Subproblemas: Crear las variables en las que estarán los inputs, crear la validación "if", concatenar los inputs en una variable nueva.
+
 str = input("Ingrese una palabra: ")
 str2 = input("Ingrese otra palabra que contenga los mismos digitos que la anterior: ")
 if len(str) != len(str2): 
