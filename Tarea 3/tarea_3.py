06d76d3d8ae2a96ca751a2a6cf6eab25a69528c4
davidangulofz@gmail.com <davidangulofz@gmail.com>
2023-02-01 19:30:52 -0600

    texto

---
+++ Tarea 3/tarea_3.py
@@ -0,0 +1,10 @@
+# Main
+print("Seleccione la operación que desea realizar")
+print("1 - Suma") 
+print("2 - Resta")
+print("3 - División")
+print("4 - Mutiplicación")
+print("5 - Factorial")
+print("6 - Potencia")
+print("7 ")
+while  elec: 
