b26d286791bc8a5a7b1c06ad0f2e4b39238da279
Andy-FM31 <andyshamalfoster@gmail.com>
2023-02-02 19:29:46 -0600

    a

--- Tarea 3/tarea_3.py
+++ Tarea 3/tarea_3.py
@@ -1,4 +1,22 @@
 # Main
+
+def suma(a,b):
+    return a+b
+
+def resta(a,b):
+    return a-b
+
+def division(a,b):
+    return a/b
+
+def multiplicacion(a,b):
+    return a*b
+
+def potencia(a,b):
+    return a**b  
+
+
+
 print("A continuación se presentan las opciones")
 print("1 - Suma") 
 print("2 - Resta")
@@ -7,5 +25,11 @@ print("4 - Mutiplicación")
 print("5 - Factorial")
 print("6 - Potencia")
 print("7 - Si desea salir")
+while 1 <= 6:
+    True
+
 while op: 
     elec = input("Seleccione el número de la operación que desea realizar")
+
+
+
