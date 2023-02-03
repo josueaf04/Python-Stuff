8201c24a08a0f1f750d3171e4a72791610d318bd
Andy-FM31 <andyshamalfoster@gmail.com>
2023-02-02 19:43:30 -0600

    NUevo

--- Tarea 3/module_ops.py
+++ Tarea 3/module_ops.py
@@ -2,31 +2,53 @@ from tarea_3 import elec
 
 rein = elec 
 
+def suma(a,b):
+    return a+b
+
+def resta(a,b):
+    return a-b
+
+def division(a,b):
+    return a/b
+
+def multiplicacion(a,b):
+    return a*b
+
+def potencia(a,b):
+    return a**b  
 while rein <=6:
     print
 
+#SUMA
+
 print("Ingrese el primer número: ") 
 a = float(input())
-print("Ingrese el segundo número: ")
+suma_total = sum([i for i in range(1, a + 1)])
+print(suma_total)
+suma(5)
+
+#RESTA
+
+print("Ingrese el primer número: ") 
+a = float(input())
+("Ingrese el segundo número: ")
 b = float(input()) 
 resta = a - b 
 print("El resultado de la resta es", resta)
+a = input("Hola, coloca un numero:")
+b = 1
 
-    a = input("Hola, coloca un numero:")
-
-    b = 1
-
-    while True:
-     for i in range(1, a+1):
-         b = (b* i)
+while True:
+ for i in range(1, a+1):
+     b = (b* i)
      if a < 0:
-            print("**ERORR EL NUMERO DEBE SER POSITIVO**")
-            print(f"Has colocado {a}")
-            break
+        print("**ERORR EL NUMERO DEBE SER POSITIVO**")
+        print(f"Has colocado {a}")
+        break
     else:
-         print("Tu numero es positivo")
-    print(f"el factorial de {a} es: {b}")
-    break
+        print(" ")
+        print(f"el factorial de {a} es: {b}")
+        break
 
 
 
