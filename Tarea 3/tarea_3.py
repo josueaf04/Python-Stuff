9d90011a9c088f3080a6214fb7be3c71d81f9502
Andy-FM31 <andyshamalfoster@gmail.com>
2023-02-02 19:45:24 -0600

    nuevo

--- Tarea 3/tarea_3.py
+++ Tarea 3/tarea_3.py
@@ -1,19 +1,6 @@
 # Main
 
-def suma(a,b):
-    return a+b
 
-def resta(a,b):
-    return a-b
-
-def division(a,b):
-    return a/b
-
-def multiplicacion(a,b):
-    return a*b
-
-def potencia(a,b):
-    return a**b  
 
 
 
