37cd08ff9a47a2374aa5c47c76176d5caf7c5935
Andy-FM31 <andyshamalfoster@gmail.com>
2023-02-05 18:20:20 -0600

    Nuevo

--- Tarea 3/module_ops.py
+++ Tarea 3/module_ops.py
@@ -1,6 +1,6 @@
 from tarea_3 import elec
 
-OP = int(elec)
+op = int(elec)
 
 
 def suma(a,b):
@@ -20,131 +20,108 @@ def potencia(a,b):
 
 
 
-<<<<<<< HEAD
+while op <= 6:
 
-# while True:
-#     print("Ingrese el primer número: ") 
-#     a = float(input())
-#     print("Ingrese el segundo número: ")
-#     b = float(input()) 
-#     suma = a + b 
-#     print(" ")
+    #SUMA
+    if op == 1:
+        while True:
+         print("Ingrese el primer número: ") 
+         a = float(input())
+         print("Ingrese el segundo número: ")
+         b = float(input()) 
+         suma = a + b 
+         print(" ")
 
-#     print("Resultado: ", suma)
+         ("Resultado: ", suma)
     
-#     print("DIGA n PARA TERMINAR, DIGA p PARA CONTINUAR")
-#     respuesta = input("Ingrese su respuesta: ")
-#     while respuesta != "n":
-#          if respuesta == "p":  
-#             print("Ingrese otro numero número: ") 
-#             a = float(input())
-#             suma = suma + a
+         print("DIGA n PARA TERMINAR, DIGA p PARA CONTINUAR")
+         respuesta = input("Ingrese su respuesta: ")
+         while respuesta != "n":
+            if respuesta == "p":  
+                print("Ingrese otro numero número: ") 
+                a = float(input())
+                suma = suma + a
 
-#             print(" ")
+                print(" ")
 
-#             print("Resultado: ", suma)
-#             print("DIGA n PARA TERMINAR, DIGA p PARA CONTINUAR")
-#             respuesta = input("¿Desea terminar el programa?: ")
+                print("Resultado: ", suma)
+                print("DIGA n PARA TERMINAR, DIGA p PARA CONTINUAR")
+                respuesta = input("¿Desea terminar el programa?: ")
 
-#     print(" ")
+            print(" ")
 
-#     print("El resultado de la suma es", suma)
-#     break
+            print("El resultado de la suma es", suma)
+            break
+
+    #RESTA
+
+    if op == 2:
+        print("Ingrese el primer número: ") 
+        a = float(input())
+        print("Ingrese el segundo número: ")
+        b = float(input()) 
+        resta = a - b 
+        print("El resultado de la resta es", resta)
         
-       
-    
-=======
-# #SUMA
-# while OP <= 6:
-#     if OP == 1:
-#         print("Ingrese el primer número: ") 
-#         a = float(input())
-#         print("Ingrese el segundo número: ")
-#         b = float(input()) 
-#         suma = a + b 
-#         print("El resultado de la resta es", suma)
->>>>>>> c6a8d0b8f7430eecb8e1e19dcdf3fcfd9a34fe93
-
-
-# RESTA
-# while OP <= 6:
-#     if OP == 2:
-#         print("Ingrese el primer número: ") 
-#         a = float(input())
-#         print("Ingrese el segundo número: ")
-#         b = float(input()) 
-#         resta = a - b 
-<<<<<<< HEAD
-#         print("El resultado de la resta es", resta)
-
-#MULTIPLICACION
-# while True:
-#     print("Ingrese el primer número: ") 
-#     a = float(input())
-#     print("Ingrese el segundo número: ")
-#     b = float(input()) 
-#     multi = a * b 
-#     print(" ")
-
-#     print("Resultado: ", multi)
-    
-#     print("DIGA n PARA TERMINAR, DIGA p PARA CONTINUAR")
-#     respuesta = input("Ingrese su respuesta: ")
-#     while respuesta != "n":
-#          if respuesta == "p":  
-#             print("Ingrese otro numero número: ") 
-#             a = float(input())
-#             multi = multi * a
-
-#             print(" ")
-
-#             print("Resultado: ", multi)
-#             print("DIGA n PARA TERMINAR, DIGA p PARA CONTINUAR")
-#             respuesta = input("¿Desea terminar el programa?: ")
-
-#     print(" ")
-
-#     print("El resultado de la suma es", multi)
-#     break
-       
-=======
-#         print("El resultado de la resta es", resta) 
-# División
-    # if OP == 3: 
-    #     print("Ingrese el primer número: ")
-    #     a = float(input())
-    #     print("Ingrese el segundo número: ")
-    #     b = float(input())
-    #     div = a / b 
-    #     print("El resultado de la división es", div)
->>>>>>> c6a8d0b8f7430eecb8e1e19dcdf3fcfd9a34fe93
-
+    #MULTIPLICACION
       
-# Factorial
-# while True:
-#  a = input("Hola, coloca un numero:")
-#         b = 1
-#  for i in range(1, a+1):
-#      b = (b* i)
-#      if a < 0:
-#         print("**ERORR EL NUMERO DEBE SER POSITIVO**")
-#         print(f"Has colocado {a}")
-#         break
-#      else:
-#         print(" ")
-#         print(f"el factorial de {a} es: {b}")
-#         break
+    if op == 3:
+        while True:
+            print("Ingrese el primer número: ") 
+            a = float(input())
+            print("Ingrese el segundo número: ")
+            b = float(input()) 
+            multi = a * b 
+            print(" ")
 
+            print("Resultado: ", multi)
+            
+            print("DIGA n PARA TERMINAR, DIGA p PARA CONTINUAR")
+            respuesta = input("Ingrese su respuesta: ")
+            while respuesta != "n":
+                if respuesta == "p":  
+                 print("Ingrese otro numero número: ") 
+                 a = float(input())
+                multi = multi * a
 
-# Potencia
-    # if OP == 6: 
-    #     print("Ingrese el primer número: ")
-    #     a = float(input())
-    #     print("Ingrese el segundo número: ")
-    #     b = float(input()) 
-    #     pot = a**b 
-    #     print("El resultado de la potencia es", pot)
+                print(" ")
 
+                print("Resultado: ", multi)
+                print("DIGA n PARA TERMINAR, DIGA p PARA CONTINUAR")
+                respuesta = input("¿Desea terminar el programa?: ")
 
-# elec ==7
-# print('JODASE')
+                print(" ")
+
+                print("El resultado de la suma es", multi)
+                break
+
+    #FACTORIAL  
+         
+    if op == 5:
+        while True:
+            a = input("Hola, coloca un numero:")
+            b = 1
+            for i in range(1, a+1):
+                b = (b* i)
+                if a < 0:
+                    print("**ERORR EL NUMERO DEBE SER POSITIVO**")
+                    print(f"Has colocado {a}")
+                break
+
+            else:
+             print(" ")
+            print(f"el factorial de {a} es: {b}")
+            break
+
+    #POTECIA
+
+    if op == 6: 
+        print("Ingrese el primer número: ")
+        a = float(input())
+        print("Ingrese el segundo número: ")
+        b = float(input()) 
+        pot = a**b 
+        print("El resultado de la potencia es", pot)
+
+op == 7
+print('JODASE')
