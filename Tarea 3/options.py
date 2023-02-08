1219618c32924aa6169be0fd21ee6b30b43ba0b4
davidangulofz@gmail.com <davidangulofz@gmail.com>
2023-02-07 20:06:33 -0600

    Finished

---
+++ Tarea 3/options.py
@@ -0,0 +1,14 @@
+# Options
+
+print("A continuación se presentan las opciones")
+print("1 - Suma") 
+print("2 - Resta")
+print("3 - Multiplicación")
+print("4 - División")
+print("5 - Factorial")
+print("6 - Potencia")
+print("7 - Si desea salir")
+elec = input("Seleccione el número de la operación que desea realizar: ")
+
+
+
