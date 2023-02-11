50ea8cb7de0d22597236455d20c3bcafebb3740d
davidangulofz@gmail.com <davidangulofz@gmail.com>
2023-02-11 12:30:57 -0600

    Idea

--- Tarea 4/ejercicio_1_t4.py
+++ Tarea 4/ejercicio_1_t4.py
@@ -1,1 +1,8 @@
 # Ejercicio 1 (función que cuenta e imprime en pantalla todos los números, letras y caracteres especiales)
+
+# def contar_caracteres(str): 
+str="P@#yn26at^&i5ve"
+for i in range(0, str+1):
+    if str.isnumeric():    
+        print("Números:", i) 
+        
