9458e87ec12f043c143e4bb1cfdfeacde365d0e2
davidangulofz@gmail.com <davidangulofz@gmail.com>
2023-02-28 13:42:16 -0600

    Created class deck

---
+++ Proyecto/deck.py
@@ -0,0 +1,24 @@
+#Clase del objecto mazo 
+
+import card  
+import random
+
+suits = ('\u2764', '\u2666', '\u2660', '\u2618')
+ranks = ('2', '3', '4', '5', '6', '7', '8', '9', 'J', 'Q', 'K', 'A')
+
+class deck: 
+    def __init__(self): 
+        self.deck = [] 
+        for suit in suits: 
+            for rank in ranks: 
+                self.deck.append(card.card(suit, rank))
+    # Función que mezcla el deck
+
+    def shuffle(self): 
+        random.shuffle(self.deck)
+    # Función que reparte las cartas 
+    def deal(self): 
+        onecard = self.deck.pop()
+        return onecard        
+
+            
