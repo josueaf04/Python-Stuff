0379b622819c14a5e2db41abf32eda7f854385fc
davidangulofz@gmail.com <davidangulofz@gmail.com>
2023-03-08 20:07:06 -0600

    Created deck class

---
+++ Project/deck.py
@@ -0,0 +1,26 @@
+#Clase del objeto deck 
+
+import card
+import random
+
+# Constantes
+suits = ('\u2764', '\u2666', '\u2660', '\u2618')
+ranks = ('2', '3', '4', '5', '6', '7', '8', '9', 'J', 'Q', 'K', 'A')
+
+class deck: 
+    def init(self): 
+        self.deck = [] 
+        for suit in suits: 
+            for rank in ranks: 
+                self.deck.append(card.card(suit, rank))
+
+# Función que mezcla el deck
+
+    def shuffle(self): 
+        random.shuffle(self.deck)
+
+# Función que reparte las cartas 
+
+    def deal(self):
+        onecard = self.deck.pop()
+        return onecard
