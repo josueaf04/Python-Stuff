2f0452a1c2501ec20f0f89ca79279aef8844df0a
davidangulofz@gmail.com <davidangulofz@gmail.com>
2023-03-08 20:06:28 -0600

    created card class

---
+++ Project/card.py
@@ -0,0 +1,13 @@
+# Clase del objeto carta
+
+
+class card:
+
+    def init(self, suit, rank): 
+        self.rank = rank
+        self.suit = suit
+
+# Función que retorna el rango y el palo de carta
+
+    def str(self): 
+        return "" .join([self.rank, self.suit])
